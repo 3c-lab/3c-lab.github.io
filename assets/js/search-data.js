8148cb50f9ab76e113bd10fcd102183704f2ca89
lcarot <87507611+lcarot@users.noreply.github.com>
2026-06-18 23:25:12 +0000

    Deploying to gh-pages from @ 3c-lab/3c-lab.github.io@870e7d5f89aec62c5f37de9076eb144436128e9e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -456,11 +456,11 @@ ninja.data = [{
           description: "Multi-parametric MRI and whole-brain imaging of AUD vulnerability and resilience",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_alcohol/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-opioid-use-disorder",
+          title: 'Opioid Use Disorder',
+          description: "Multi-parametric MRI and whole-brain imaging of oxycodone addiction-like behaviors",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
+              window.location.href = "/projects/2_opioid/";
             },},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
