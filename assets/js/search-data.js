218eddedcf6e2fc39362b8cc0c1eea5198144987
lcarot <87507611+lcarot@users.noreply.github.com>
2026-06-19 01:09:58 +0000

    Deploying to gh-pages from @ 3c-lab/3c-lab.github.io@8ef2750ace75d6f4de9cc47d8e20dce643071e4c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -58,7 +58,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/alumni/";
           },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
+        },{id: "post-notice-of-award-niaaa-r01aa031958",
+        
+          title: "Notice of Award — NIAAA R01AA031958",
+        
+        description: "Magnetic Resonance Imaging of Outbred Rats with Variable Vulnerability to Compulsive Alcohol Use",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/niaaa-award/";
+          
+        },
+      },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
         
           title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
         
