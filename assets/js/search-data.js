f8be02d2e277eda22f59ee4c8ab81b81fffbcd36
lcarot <87507611+lcarot@users.noreply.github.com>
2026-06-19 01:38:18 +0000

    Deploying to gh-pages from @ 3c-lab/3c-lab.github.io@d1694502e172397a8d29c561bd22b837177e9c0c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -129,15 +129,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-alcohol-use-disorder",
+            },},{id: "projects-alcohol-use-disorder",
           title: 'Alcohol Use Disorder',
           description: "Multi-parametric MRI and whole-brain imaging of AUD vulnerability and resilience",
           section: "Projects",handler: () => {
